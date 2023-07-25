d70ffdc4f72f1b5b599774b7478fa4bc0daf7df9
paulonc <paulo.cavalcanti@ccc.ufcg.edu.br>
2023-07-25 20:49:58 -0300

    implementação do backend

---
+++ api/server.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	db "github.com/paulonc/go-products/db/sqlc"
+)
+
+type Server struct {
+	store  *db.ExecuteStore
+	router *gin.Engine
+}
+
+func InstanceServer(store *db.ExecuteStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// router.POST("/product", server.createProduct)
+	// router.GET("/product/:id", server.getProduct)
+	// router.GET("/products", server.getProducts)
+	// router.PUT("/product", server.updateProduct)
+	// router.DELETE("/product/:id", server.deleteProduct)
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"api has one error:": err.Error()}
+}
